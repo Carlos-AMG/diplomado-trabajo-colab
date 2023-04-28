33775deb488d67ff9c4a0a9546c4814a9abbc2b2
ericmtzr <ericmtzr89@gmailcom>
2023-04-27 18:13:01 -0600

    adding more changes

--- _uptime/uptime.h
+++ _uptime/uptime.h
@@ -1,3 +1,1 @@
-#include "platform.h"
-
 int uptime_main(int argc UNUSED_PARAM, char **argv UNUSED_PARAM);
--- main.c
+++ main.c
@@ -4,12 +4,7 @@
 #include <ctype.h>
 # include <error.h>
 
-#include "main.h"
-#include "sleep.h"
-#include "uname.h"
-#include "ls.h"
 #include "uptime.h"
-#include "lsmod.h"
 
 struct buildcmd {
 	const char *namecmd;
@@ -17,30 +12,17 @@ struct buildcmd {
 	int (*fncmd)(int, char **);
 };
 
-
-static int sleepcmd(int argc, char **argv)  { return sleep_main(argc, argv); }
-static int unamecmd(int argc, char **argv)  { return uname_main(argc, argv); }
-static int lscmd(int argc, char **argv)  { return ls_main(argc, argv); }
 static int uptimecmd(int argc, char **argv)  { return uptime_main(argc, argv); }
-static int lsmod(int argc, char **argv)  { return lsmod_main(argc, argv); }
 
 /*Enum with the commands*/
 enum allcmd{
-    SLEEP_CMD,
-    UNAME_CMD,
-    LS_CMD,
     UPTIME_CMD,
-    LSMOD_CMD,
     TOTAL_CMD
 };
 
 /*struct with the commands*/
 static const struct buildcmd listcmd[TOTAL_CMD] = {
-	{"sleep"    , 1 , sleepcmd  },
-    {"uname"    , 0 , unamecmd  },
-    {"ls"       , 1 , lscmd     },
-    {"uptime"   , 0 , uptimecmd },
-    {"lsmod"    , 0 , lsmod     },
+    {"_uptime"   , 0 , uptimecmd },
 };
 
 
